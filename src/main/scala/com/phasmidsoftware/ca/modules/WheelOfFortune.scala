b1a569d7e29c86abb06d41138d6aa56d52c982db
Robin Hillyard <r.hillyard@northeastern.edu>
2023-02-17 13:25:29 -0500

    Added WheelOfFortune.scala

--- src/main/scala/com/phasmidsoftware/ca/modules/WheelOfFortune.scala
+++ src/main/scala/com/phasmidsoftware/ca/modules/WheelOfFortune.scala
@@ -2,19 +2,29 @@ package com.phasmidsoftware.ca.modules
 
 import scala.util.Random
 
-case class WheelOfFortune[X](m: Map[X, Int])(implicit random: Random) {
+case class WheelOfFortune[T](map: Map[T, Int])(implicit random: Random) {
     // The total of all the (weighting) values corresponds to 1.
     // And thus the probability of any event happening is the weighting over the total weights.
-    def next: X = ???
+    val (events, odds) = map.toSeq.unzip
+    private val total = odds.sum
+    val accumulated: Map[T, Int] = (events zip (odds.scanLeft(0)((a, x) => a + x))).toMap
+
+    def next: T = {
+        val n = random.nextInt(total)
+
+        def tooSmall(t: (T, Int)): Boolean = t._2 < n
+
+        (accumulated dropWhile tooSmall).head._1
+    }
 
 }
 
 object WheelOfFortune extends App {
     implicit val random = new Random
 
-    def createFromSeq[X](m: Seq[Tuple2[X, Int]]): WheelOfFortune[X] = new WheelOfFortune[X](m.toMap)
+    def createFromSeq[X](m: Seq[Tuple2[X, Int]])(implicit random: Random): WheelOfFortune[X] = new WheelOfFortune[X](m.toMap)
 
-    def create[X](m: Tuple2[X, Int]*): WheelOfFortune[X] = createFromSeq[X](m)
+    def create[X](m: Tuple2[X, Int]*)(implicit random: Random): WheelOfFortune[X] = createFromSeq[X](m)
 
     val wheelOfFortune = create[Boolean](true -> 1, false -> 1)
     wheelOfFortune.next
--- src/test/scala/com/phasmidsoftware/ca/modules/WheelOfFortuneSpec.scala
+++ src/test/scala/com/phasmidsoftware/ca/modules/WheelOfFortuneSpec.scala
@@ -8,16 +8,25 @@ class WheelOfFortuneSpec extends AnyFlatSpec with should.Matchers {
 
     behavior of "WheelOfFortune"
 
-    it should "createFromSeq" in {
+    it should "createFromSeq Boolean" in {
         implicit val random: Random = new Random(0)
         val wheelOfFortune = WheelOfFortune.createFromSeq[Boolean](List(true -> 1, false -> 1))
-        wheelOfFortune.next shouldBe true
+        wheelOfFortune.next shouldBe false
     }
 
-    it should "create" in {
+    it should "create Boolean" in {
         implicit val random: Random = new Random(0)
         val wheelOfFortune = WheelOfFortune.create[Boolean](true -> 1, false -> 1)
-        wheelOfFortune.next shouldBe true
+        wheelOfFortune.next shouldBe false
     }
+
+    it should "create Int" in {
+        implicit val random: Random = new Random(0)
+        val wheelOfFortune = WheelOfFortune.create[Int](1 -> 1, 2 -> 1, 3 -> 1)
+        wheelOfFortune.next shouldBe 1
+        wheelOfFortune.next shouldBe 2
+        wheelOfFortune.next shouldBe 2
+    }
+
 
 }
